e02b3f323394570014567da7a74b013ae8f1ac02
Артем bc2025-6 <artemmordastik@gmail.com>
2025-11-30 22:37:13 +0200

    Оновлено файли підготовки до роботи

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("Проєкт розпочато.");
+import { Command } from 'commander';
+
+// Створення нового об'єкта програми
+const program = new Command();
+
+// Налаштування інформації про програму
+program
+  .name('backend-app')
+  .description('A simple backend application for course 2025-6')
+  .version('1.0.0');
+
+// Додавання команди
+program.command('greet')
+  .description('Say hello to a specified person')
+  .argument('<name>', 'Name of the person to greet')
+  .action((name) => {
+    console.log(`Привіт, ${name}! Ласкаво просимо до курсу!`);
+  });
+
+// Парсинг аргументів командного рядка
+program.parse(process.argv);
